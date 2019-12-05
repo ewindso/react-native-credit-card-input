dcf5fad1cfa241de1d5fa2bd5faef2fe5da13c8e
Elijah Windsor <ewindsor@gmail.com>
2019-12-05 11:16:34 -0500

    Exporting VerticalCardInput

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import connectToState from "./connectToState";
 import CCF from "./CreditCardInput";
+import VCCF from './VerticalCardInput'
 import LiteCCF from "./LiteCreditCardInput";
 import CV from "./CardView";
 
 export const CreditCardInput = connectToState(CCF);
+export const VerticalCardInput = connectToState(VCCF)
 export const LiteCreditCardInput = connectToState(LiteCCF);
 export const CardView = CV;
